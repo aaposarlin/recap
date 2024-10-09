8539560e698b2ba1f13c6c956602e9640c790500
aaposarlin <aapo.sarlin@hotmail.fi>
2024-10-09 10:06:02 +0300

    Added functions to calculate, get results

--- recap-tehtava/src/CalcForm.jsx
+++ recap-tehtava/src/CalcForm.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
+
 function CalcForm() {
+const [firstNumber, setFirstNumber] = useState('');
+const [secondNumber, setSecondNumber] = useState('');
+const [result, setResult] = useState(null);
+const calculate = () => {
+    setResult(parseFloat(firstNumber) + parseFloat(secondNumber));
+}
+
+
+
         return (
-            <><input></input><p></p>
+            <><input type="number" value={firstNumber} onChange={(e)=>setFirstNumber(e.target.value)}></input>
+            <p></p>
              <select>
             <option value="plus">+</option>
-            <option value="minus">-</option>
-            <option value="multi">*</option>
-            <option value="divide">/</option>
-            </select><p></p>
-            <input></input></>
+            </select>
+            <p></p>
+            <input type="number" value={secondNumber} onChange={(e)=>setSecondNumber(e.target.value)}></input>
+            <p></p>
+            <button onClick={calculate}>Calculate</button>
+            <p>Sum of numbers {firstNumber} and {secondNumber} is {result}</p>
+            </>
     
         )
     }
