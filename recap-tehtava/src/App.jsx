a18033b586e9d1c488be0964d9f5888aebd1e0c4
aaposarlin <aapo.sarlin@hotmail.fi>
2024-10-09 09:23:59 +0300

    Added Calcform input and options

--- recap-tehtava/src/App.jsx
+++ recap-tehtava/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./header"
+import CalcForm from "./CalcForm"
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Header />
+      <CalcForm />
     </div>
   )
 }
---
+++ recap-tehtava/src/CalcForm.jsx
@@ -0,0 +1,15 @@
+function CalcForm() {
+        return (
+            <><input></input><p></p>
+             <select>
+            <option value="plus">+</option>
+            <option value="minus">-</option>
+            <option value="multi">*</option>
+            <option value="divide">/</option>
+            </select><p></p>
+            <input></input></>
+    
+        )
+    }
+
+export default CalcForm
